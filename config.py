1a9b01cd8717f54fa299f67a42359745d3fb85e5
kauevestena <kauemv2@gmail.com>
2023-04-05 11:04:36 -0300

    boundaries ok

---
+++ config.py
@@ -0,0 +1,9 @@
+'''
+    file containing constants in order to ease the process of reproduce te analysis undertaken
+'''
+
+CITY_SHORTNAME = 'curitba' #without spaces or uppercase characters
+CITY_DESCRIPTION = 'Curitiba, Brazil'
+
+# Key, description for Nominatim
+NEIGHBORHOODS = {'agua_verde':'Água Verde, Curitiba','jardim_das_americas':'Jardim das Américas, Curitiba'}
---
+++ functions.py
@@ -0,0 +1,48 @@
+import osmnx as ox
+import geopandas as gpd
+import json
+import Levenshtein
+
+def features_from_place(place_name, tags):
+    features = ox.geometries.geometries_from_place(place_name, tags=tags)
+    return features
+
+
+def features_to_file(place_name, tags,outpath):
+    gdf = features_from_place(place_name,tags)
+
+    transform_list_cols_to_str(gdf)
+
+    gdf.to_file(outpath)
+
+def transform_list_cols_to_str(df):
+    # to correct the problem with 
+    for col in df.columns:
+        if df[col].dtype == 'object':
+            df[col] = df[col].apply(lambda x: str(x))
+
+
+def gdf_to_file(gdf,outpath):
+    transform_list_cols_to_str(gdf)
+    gdf.to_file(outpath)
+
+
+def read_json(inputpath):
+    with open(inputpath) as reader:
+        data = reader.read()
+
+    return json.loads(data)
+    
+def dump_json(inputdict,outputpath,indent=4):
+    with open(outputpath,'w+',encoding='utf8') as json_handle:
+        json.dump(inputdict,json_handle,indent=indent,ensure_ascii=False)
+
+def most_similar_string(string_list,target_string):
+    return min(string_list, key=lambda x: Levenshtein.distance(x, target_string))
+
+def most_similar_string_in_df(df,column,target_string):
+    return most_similar_string(list(df[column].fillna('NULL')),target_string)
+
+def save_gdf_row_by_match(gdf,colum,value,outputpath):
+    row = gdf[gdf[colum] == value]
+    gdf_to_file(row,outputpath)
